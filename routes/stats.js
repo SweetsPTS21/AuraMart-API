777f4952d8048760a1b82435fa0bf013b18f2765
Pham Thanh Son <pts.28032001@gmail.com>
2023-12-04 17:43:07 +0700

    fix stats

--- routes/stats.js
+++ routes/stats.js
@@ -4,9 +4,8 @@ const { getTopSoldProduct, getTopSoldOfShop } = require('../controller/stats');
 const { protect, authorize } = require('../middleware/auth');
 
 router.use(protect);
-router.use(authorize('admin'));
 
-router.route('/').get(getTopSoldOfShop);
+router.route('/').get(authorize("admin"), getTopSoldOfShop);
 router.route('/products').get(getTopSoldProduct);
 
 module.exports = router;
